ea5be03b9ee31e16f421b048bc370b3bd81aa8bb
Parvathy Nair <parvathy.meetu@gmail.com>
2026-03-14 22:07:03 +0530

    Initial Selenium Maven Framework

---
+++ src/main/java/selenium/LaunchChromeDriver.java
@@ -0,0 +1,9 @@
+package selenium;
+
+class LaunchChromeDriverMain {
+
+    public static void main(String[] args) {
+        System.out.println("This is a simple runner. Use the test class under src/test/java with TestNG to run automated tests.");
+    }
+
+}
---
+++ src/test/java/selenium/LaunchChromeDriver.java
@@ -0,0 +1,54 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchChromeDriver {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+    }
+
+    @Test(priority = 1)
+    public void testTitle() {
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+        Assert.assertTrue(title.toLowerCase().contains("google"), "Title should contain 'Google'");
+    }
+
+    @Test(priority = 2)
+    public void testSearchBoxPresent() {
+        boolean present = driver.findElements(By.name("q")).size() > 0;
+        Assert.assertTrue(present, "Search box should be present on Google home page");
+    }
+
+    @Test(priority = 3)
+    public void testNavigateToAbout() {
+        try {
+            driver.findElement(By.linkText("About")).click();
+        } catch (Exception e) {
+            driver.get("https://about.google/");
+        }
+        Assert.assertTrue(driver.getCurrentUrl().contains("about"), "Should navigate to an About page");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
